da6b0d8e33246dea2cd805f2d25443e20395cbab
Anna Bakas <abakas@luc.edu>
2020-10-16 16:24:36 -0500

    formatting

--- player.c
+++ player.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void init_first_player(player_t *first){
-	char name[NAMELEN+1];
+void init_first_player(player_t *first) {
+    char name[NAMELEN+1];
 
-	printf("Enter name of Player 1: ");
-	scanf("%s",name);
-	strncpy(first -> name, name, NAMELEN);
-	first -> token = 'X';
+    printf("Enter name of Player 1: ");
+    scanf("%s",name);
+    strncpy(first -> name, name, NAMELEN);
+    first -> token = 'X';
 }
 
