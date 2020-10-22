54c9772e73d15b7cdac71dab4175912305b0cb53
Anna Bakas <abakas@luc.edu>
2020-10-21 19:20:06 -0500

    starting game and prompting coordinates from user

--- gameboard_demo.c
+++ gameboard_demo.c
@@ -10,15 +10,42 @@ int main(void) {
     int start = 1;
     int number_moves = 0;
     int invalid_moves = 0;
+    int row, col;
+    char again = 0;
     int moves[SIZE][SIZE] = {0};
-    do {
-        //Player A goes first
-        if(start++ % 2 == 0) {
-            printf("It's Player A's Turn\n");
-            if(valid_moves(&board,moves,PLAYER_A)){
-	    }
-        }
 
-    }while(number_moves < SIZE*SIZE && invalid_moves < 0);
+    othello_new(&board);
+    number_moves = 4;
+    //do {
+    //Player A goes first
+    if(start++ % 2) {
+        printf("It's Player A's Turn\n");
+        displayBoard(&board);
+        if(valid_moves(&board,moves,PLAYER_A)) {
+            prompt_move(&row, &col);
+            row--;
+            col--;
+
+            if(row >= 0 && row < SIZE && col >= 0 && col < SIZE) {
+                number_moves++;
+                //break;
+            }
+            else {
+                printf("Those coordinates are not valid. Please try again!\n");
+            }
+        }
+        else {
+            if(++invalid_moves < 2) {
+                fflush(stdin);
+                printf("You have to pass. Please press enter");
+                scanf("%c", &again);
+            }
+            else {
+                printf("Neither us us can go. Game Over.\n");
+            }
+        }
+    }
+
+    //}while(number_moves < SIZE*SIZE && invalid_moves < 0);
 
 }
