9c8bfa1836cef7980557fe13d466c73bd42b0bfb
Anna Bakas <abakas@luc.edu>
2020-10-27 15:29:05 -0500

    flip_pieces() function

--- logic.h
+++ logic.h
@@ -10,3 +10,4 @@ extern void make_move(othello_board_t *board, int r, int c, char player);
 extern char get_winner(int playerA_Score, int playerB_Score);
 extern int off_board(int x, int y);
 extern int found_blank(othello_board_t *board, int x, int y);
+extern void flip_pieces(othello_board_t *board, int x, int y, int row_count, int col_count, char other, char player);
