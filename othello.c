ea0c542eabbcab5049a244601de7470d3ef400d0
Anna Bakas <abakas@luc.edu>
2020-10-28 15:10:22 -0500

    converted lines of code to different functions

--- othello.c
+++ othello.c
@@ -30,18 +30,18 @@ int main(void) {
     do {
         playerA_Score = getScore(&board, player);
         playerB_Score = getScore(&board, other);
-        printf("\nPlayer A's Score: %d\n", playerA_Score);
-        printf("Player B's Score: %d\n\n", playerB_Score);
+        display_score(playerA_Score, playerB_Score);
 
         if(start % 2 == 0) {
-            printf("It's Player %c's Turn\n", player);
-            //printf("Possible Moves: \n");
+            display_current_player(player);
             if(valid_moves(&board,moves,player)) {
                 displayBoard(&board);
                 for(;;) {
                     prompt_move(&row, &col);
-                    row--;  //Decrement user input by one to get row/col index
-                    col--;
+
+                    //Decrement row and column input to get index value
+                    row = get_index(row);
+                    col = get_index(col);
 
                     //If position entered is valid:
                     //Put move on board
@@ -54,15 +54,13 @@ int main(void) {
                         break;
                     }
                     else {
-                        printf("Those coordinates are not valid. Please try again!\n");
+                        display_invalid_coords();
                     }
                 }
             }
             else {
                 if(++invalid_moves < 2) {
-                    fflush(stdin);
-                    printf("You have to pass. Please press enter");
-                    scanf("%c", &again);
+                    pass_move(&again);
                 }
                 else {
                     printf("Neither us us can go. Game Over.\n");
@@ -70,14 +68,15 @@ int main(void) {
             }
         }
         else {
-            printf("It's Player %c's Turn\n", other);
-            //printf("Possible Moves: \n");
+            display_current_player(other);
             if(valid_moves(&board,moves,other)) {
                 displayBoard(&board);
                 for(;;) {
                     prompt_move(&row, &col);
-                    row--;  //Decrement user input by one to get row/col index
-                    col--;
+
+                    //Decrement row and column input by one to get index value
+                    row = get_index(row);
+                    col = get_index(col);
 
                     //If position entered is valid:
                     //Put move on board
@@ -90,15 +89,13 @@ int main(void) {
                         break;
                     }
                     else {
-                        printf("Those coordinates are not valid. Please try again!\n");
+                        display_invalid_coords();
                     }
                 }
             }
             else {
                 if(++invalid_moves < 2) {
-                    fflush(stdin);
-                    printf("You have to pass. Please press enter");
-                    scanf("%c", &again);
+                    pass_move(&again);
                 }
                 else {
                     printf("Neither us us can go. Game Over.\n");
@@ -106,7 +103,7 @@ int main(void) {
             }
         }
         start++; // Switching between player A and player B
-    } while(total_moves < SIZE*SIZE && invalid_moves<2);
+    } while(quit(&board, invalid_moves) == 1);
 
     printf("\nCongrats Player %c, you won!\n",get_winner(playerA_Score, playerB_Score));
 }
