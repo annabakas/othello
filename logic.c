43c5238c295de7c6a378211e46a7e45a8d8d4b90
Anna Bakas <abakas@luc.edu>
2020-10-29 15:47:10 -0500

    comments and more functions

--- logic.c
+++ logic.c
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+//Returns 1 if search has moved off the board
+//Returns 0 if search is still on the board
 int off_board(int x, int y) {
     if(x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
         return 1;
@@ -16,6 +18,8 @@ int off_board(int x, int y) {
     }
 }
 
+//Returns 1 if a blank square was encountered
+//Returns 0 if a filled square was encountered
 int found_blank(othello_board_t *board, int x, int y) {
     if(board -> board[x][y] == EMPTY) {
         return 1;
@@ -25,16 +29,35 @@ int found_blank(othello_board_t *board, int x, int y) {
     }
 }
 
+
+//Backtracks from current player's piece
+//If other player's piece is found while backtracking
+//Flip their pieces to current player's piece
 void flip_pieces(othello_board_t *board, int x, int y, int row_count, int col_count, char other, char player) {
     while(board -> board[x-=row_count][y-=col_count] == other) {
         board -> board[x][y] = player;
     }
 }
 
-void place_piece(othello_board_t *board, int r, int c, char player){
-	board -> board[r][c] = player;
+//Places current player piece on the board
+void place_piece(othello_board_t *board, int r, int c, char player) {
+    board -> board[r][c] = player;
 }
 
+//Update row and column search indices with row/col indices and row/col indices around a square
+void move_to_opponent(int x, int r, int row_count, int y, int c, int col_count) {
+    x = r + row_count;
+    y = c + col_count;
+}
+
+//Update row and column search indices with row and column indices around a square
+void move_to_next(int x, int y, int row_count, int col_count) {
+    x += row_count;
+    y += col_count;
+}
+
+//Places current player's piece
+//Flips opponent's pieces to player's pieces
 void make_move(othello_board_t *board, int r, int c, char player) {
     int row_count = 0;
     int col_count = 0;
@@ -59,12 +82,10 @@ void make_move(othello_board_t *board, int r, int c, char player) {
             //direction for player's piece so we know which pieces
             //need to be flipped
             if(board -> board[r + row_count][c + col_count] == other) {
-                x = r + row_count;
-                y = c + col_count;
+                move_to_opponent(x, r, row_count, y, c, col_count);
 
                 for(;;) {
-                    x += row_count;
-                    y += col_count;
+                    move_to_next(x, y, row_count, col_count);
 
                     //Move off the board? Break
                     if(off_board(x,y) == 1) {
@@ -95,4 +116,22 @@ char get_winner(int playerA_Score, int playerB_Score) {
     else {
         return PLAYER_B;
     }
+}
+
+//Return 1 if board is not full and invalid moves are less than 2
+//Quit gameplay loop is quit() returns 0
+int quit(othello_board_t *board, int invalid_moves) {
+    if((board_full(board) != 1) && invalid_moves < 2) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+//Passes in row/column input by user and decrements it by 1 to get index value
+//Returns decremented value
+int get_index(int x) {
+    x--;
+    return x;
 }
