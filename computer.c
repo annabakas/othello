fbc5e09705a299e2e34ecbc96b935dde255a9324
Anna Bakas <60755105+annabakas@users.noreply.github.com>
2020-11-02 12:21:37 -0500

    Create computer.c

---
+++ computer.c
@@ -0,0 +1,10 @@
+#include "logic.h"
+#include "player.h"
+#include "gameboard.h"
+#include "utility.h"
+#include "computer.h"
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
