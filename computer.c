ea5152ba2d02711fd620eb3af7d0c9bd7f21e202
Anna Bakas <abakas@luc.edu>
2020-11-04 11:35:30 -0600

    changed copyBoard and starting lookahead

--- computer.c
+++ computer.c
@@ -10,12 +10,14 @@
 #include <stdlib.h>
 
 //Make copy of board
-void copy_board(othello_board_t *board, char tempBoard[SIZE][SIZE]) {
-    for(int row = 0; row < SIZE; row++) {
+void copy_board(othello_board_t *board, othello_board_t *tempBoard) {
+    /*for(int row = 0; row < SIZE; row++) {
         for(int col = 0; col < SIZE; col++) {
-            tempBoard[row][col] = board -> board[row][col];
+            tempBoard -> tempBoard[row][col] = board -> board[row][col];
         }
-    }
+    }*/
+
+    tempBoard = board;
 }
 
 //Returns current player's score minus opponent's score
@@ -25,3 +27,41 @@ int heuristic(othello_board_t *board, char player) {
     int opponentScore = getScore(board, opponent);
     return (playerScore - opponentScore);
 }
+
+void decision(othello_board_t *board, int moves[][SIZE], char player) {
+    int numMoves;
+    char opponent = set_opponent(player);
+    othello_board_t tempBoard;
+    int tempMoves[SIZE][SIZE];
+    int row, col;
+    int moveX[60], moveY[60];
+
+
+    numMoves = valid_moves(&tempBoard, tempMoves, player);
+
+    if(numMoves == 0) {
+        row = -1;
+        col = -1;
+    }
+    else {
+        int bestMoveVal = -99999;
+        int bestX = moveX[60], bestY = moveY[0];
+
+        for(int i = 0; i < numMoves; i++) {
+            copy_board(board, &tempBoard);
+            make_move(tempBoard, row, col, player);
+
+            int val = minimaxVal(&tempBoard, player, opponent, 1);
+
+            if(val > bestMoveVal) {
+                bestMoveVal = val;
+                bestX = moveX[i];
+                bestY = moveY[i];
+            }
+        }
+        x = bestX;
+        y = bestY;
+    }
+}
+
+
--- computer.h
+++ computer.h
@@ -11,7 +11,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void copy_board(othello_board_t *board, char tempBoard[SIZE][SIZE]);
+void copy_board(othello_board_t *board, othello_board_t *tempBoard);
 int heuristic(othello_board_t *board, char player);
-
+void decision(othello_board_t *board, int moves[][SIZE], char player);
 #endif
