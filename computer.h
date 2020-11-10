3c48f479906c25f0067d6d52553a4ba0e86f7fae
Anna Bakas <abakas@luc.edu>
2020-11-10 13:49:21 -0600

    computer makes move where  next turn (human) is smallest

--- computer.h
+++ computer.h
@@ -14,7 +14,6 @@
 void copy_board(othello_board_t *board, othello_board_t *tempBoard);
 int heuristic(othello_board_t *board, char player);
 int best(othello_board_t *board, int moves[][SIZE], char player, int search);
-void assign_best(int new_score, int score, int best_row, int row, int best_col, int col);
 int set_high(int tempScore, int max);
 void decision(othello_board_t *board, int moves[][SIZE], char player);
 #endif
